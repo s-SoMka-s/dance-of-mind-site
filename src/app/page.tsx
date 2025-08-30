7a7f3cfddbdca3d5308440ac7c3a667ec08ec65c
Игорь Чиёсов <i.chiesov@gmail.com>
2025-08-30 23:38:06 +0700

    Redirect to first.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
-'use client';
+import { redirect } from 'next/navigation';
 
 export default function Home() {
-  return (
-    <div className="h-full w-full bg-black flex flex-col items-center justify-center overflow-hidden"></div>
-  );
+  redirect('/comedy-tragedy');
 }
