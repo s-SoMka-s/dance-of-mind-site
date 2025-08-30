8ff040fdf0047644c9ade4f810237bd8aa883915
Игорь Чиёсов <i.chiesov@gmail.com>
2025-08-30 23:30:43 +0700

    Commet card..

--- src/app/who-i-am/page.tsx
+++ src/app/who-i-am/page.tsx
@@ -1,12 +1,7 @@
-'use client';
-
 import { PARTICLES } from '@/app/comedy-tragedy/constants';
 import { SparklesCore } from '@/components/ui/sparkles';
-import { TextGenerateEffect } from '@/components/ui/text-generate-effect';
 
 export default function WhoIAmPage() {
-  const words = 'Кто я?';
-
   return (
     <div className="relative min-h-screen w-full bg-black flex flex-col items-center justify-center overflow-x-hidden">
       <div className="w-full absolute inset-0">
@@ -22,9 +17,7 @@ export default function WhoIAmPage() {
         />
       </div>
 
-      <div className="relative z-10 flex flex-col gap-6 items-center w-full max-w-5xl px-4">
-        <TextGenerateEffect words={words}></TextGenerateEffect>
-      </div>
+      <div className="relative z-10 flex flex-col items-center w-full max-w-5xl px-4"></div>
     </div>
   );
 }
