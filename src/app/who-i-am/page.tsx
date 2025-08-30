fae248be43f434aceaa27b509015c9abafe61129
Игорь Чиёсов <i.chiesov@gmail.com>
2025-08-30 23:20:08 +0700

    Who i am text.

--- src/app/who-i-am/page.tsx
+++ src/app/who-i-am/page.tsx
@@ -1,7 +1,30 @@
+'use client';
+
+import { PARTICLES } from '@/app/comedy-tragedy/constants';
+import { SparklesCore } from '@/components/ui/sparkles';
+import { TextGenerateEffect } from '@/components/ui/text-generate-effect';
+
 export default function WhoIAmPage() {
+  const words = 'Кто я?';
+
   return (
-    <div className="min-h-screen w-full bg-black flex items-center justify-center">
-      <h1 className="text-white text-4xl font-semibold tracking-wide">who i am</h1>
+    <div className="relative min-h-screen w-full bg-black flex flex-col items-center justify-center overflow-x-hidden">
+      <div className="w-full absolute inset-0">
+        <SparklesCore
+          id="tsparticles-comedy-tragedy"
+          background="transparent"
+          minSize={PARTICLES.minSize}
+          maxSize={PARTICLES.maxSize}
+          speed={PARTICLES.defaultSpeed}
+          particleDensity={PARTICLES.defaultDensity}
+          className="w-full h-full"
+          particleColor={PARTICLES.color}
+        />
+      </div>
+
+      <div className="relative z-10 flex flex-col gap-6 items-center w-full max-w-5xl px-4">
+        <TextGenerateEffect words={words}></TextGenerateEffect>
+      </div>
     </div>
   );
 }
